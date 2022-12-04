c881a93b3101b9d2c8d13ec2b7b49f1cef84fcca
Srujan Papaiahgari <19625909+srujanpapaiah@users.noreply.github.com>
2022-12-04 10:36:11 +0530

    product schema added

--- models/product.schema.js
+++ models/product.schema.js
@@ -1,7 +1,46 @@
 import mongoose, { mongo } from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a product name"],
+      trim: true,
+      maxlenght: [120, "Product name should be a max of 120 characters"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please provide a product price"],
+      maxlenght: [5, "Product price should not exceed 5 digits"],
+    },
+    description: {
+      type: String,
+      // use some form of editor - personla assignment
+    },
+    photos: [
+      {
+        secure_url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    collectionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Collection",
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Product", productSchema);
