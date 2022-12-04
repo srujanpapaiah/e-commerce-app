d2d7abade7371e9b7492dd65ee9b33cf961fa4ca
Srujan Papaiahgari <19625909+srujanpapaiah@users.noreply.github.com>
2022-12-04 12:24:55 +0530

    AuthController - login, logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,15 +8,13 @@ export const cookieOptions = {
   //could be in a seperate file in utils
 };
 
-/****************************** 
-
-* @SIGNUP
-* @route http://localhost:4000/api/auth/signup
-* @description User signup controller for creating a new user
-* @parameters name, email, password
-* @return User Object
-
-*********************************/
+/******************************
+ * @SIGNUP
+ * @route http://localhost:4000/api/auth/signup
+ * @description User signup controller for creating a new user
+ * @parameters name, email, password
+ * @return User Object
+ *********************************/
 
 export const signUp = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -48,5 +46,62 @@ export const signUp = asyncHandler(async (req, res) => {
     success: true,
     token,
     user,
+  });
+});
+
+/******************************
+ * @LOGIN
+ * @route http://localhost:4000/api/auth/login
+ * @description User signIn controller for logging new user
+ * @parameters email, password
+ * @return User Object
+ *********************************/
+
+export const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomError("Please fill all fields", 400);
+  }
+
+  const user = User.findOne({ email }).select("+password");
+
+  if (!user) {
+    throw new CustomError("Invalid credentials", 400);
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (isPasswordMatched) {
+    const token = user.getJwtToken();
+    user.password = undefined;
+    res.cookie("token", token, cookieOptions);
+    return res.status(200).json({
+      success: true,
+      token,
+      user,
+    });
+  }
+
+  throw new CustomError("Invalid Credentials", 400);
+});
+
+/******************************
+ * @LOGOUT
+ * @route http://localhost:4000/api/auth/logout
+ * @description User Logoutby clearing user cookies
+ * @parameters
+ * @return Success message
+ *********************************/
+
+export const logout = asyncHandler(async (_req, res) => {
+  // res.clearCookie()
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Logged out",
   });
 });
